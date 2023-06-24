3b23d16a1e274d5d31a79d89029985fceea484cf
kai.guo <gkmails@126.com>
2023-06-24 23:17:15 +0800

    提交所有文件

---
+++ src/opengl.cpp
@@ -0,0 +1,60 @@
+﻿// opengl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "glm/glm.hpp"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "iostream"
+
+void init(GLFWwindow* window)
+{
+
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+    glClearColor(1.0, 0.5, 1.0, 0.6);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+
+
+
+int main()
+{
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - programe1", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+    {
+        exit(EXIT_FAILURE);
+    }
+    init(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        display(window,glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
