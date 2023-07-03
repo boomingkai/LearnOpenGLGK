810c3c3bb17955a9bd6b507e49d5cf5201f1afd6
kai.guo <gkmails@126.com>
2023-07-03 23:41:03 +0800

    使用矩阵栈绘制太阳、地球、月球运动轨道

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,26 @@
 #include <glm/gtc/type_ptr.hpp> // glm::value_ptr
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include "Utils.h"
+#include <stack>
 using namespace std;
 
 #define numVAOs 1
-#define numVBOs 1
+#define numVBOs 2
 
 float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+float pyrLocX, pyrLocY, pyrLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // variable allocation for display
-GLuint mLoc, vLoc, projLoc, tfLoc;
+GLuint mvLoc, projLoc;
 int width, height;
-float aspect, timeFactor;
+float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
 
-void setupVertices(void)
-{
+void setupVertices(void) {
 	float vertexPositions[108] =
 	{ -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
 		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
@@ -40,13 +42,22 @@ void setupVertices(void)
 		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
 		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
 	};
-
+	float pyramidPositions[54] =
+	{ -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,    //front
+		1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,    //right
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  //back
+		-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //left
+		-1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, //LF
+		1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f  //RR
+	};
 	glGenVertexArrays(1, vao);
 	glBindVertexArray(vao[0]);
 	glGenBuffers(numVBOs, vbo);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);
 }
 
 void init(GLFWwindow* window) {
@@ -56,37 +67,78 @@ void init(GLFWwindow* window) {
 	aspect = (float)width / (float)height;
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
 
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 420.0f; // Z=32.0f when 24 instances, 420.0f when 100000 instances
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 900.0f;
+	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+	pyrLocX = 2.0f; pyrLocY = 2.0f; pyrLocZ = 0.0f;
 	setupVertices();
 }
 
+stack<glm::mat4> mvStack;
 void display(GLFWwindow* window, double currentTime) {
 	glClear(GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+
 	glUseProgram(renderingProgram);
 
-	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-
-	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-
-	timeFactor = ((float)currentTime);
-	tfLoc = glGetUniformLocation(renderingProgram, "tf");
-	glUniform1f(tfLoc, (float)timeFactor);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	// 太阳
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mvStack.push(vMat);
+	mvStack.push(mvStack.top());
+	// 太阳公转(为0)
+	mvStack.top() *= glm::translate(glm::mat4(1.0), glm::vec3(0.0, 0.0, 0.0));
+	mvStack.push(mvStack.top());
+	// 基于太阳公转的位置再计算自转(旋转矩阵)
+	mvStack.top() *= glm::rotate(glm::mat4(1.0), (float)currentTime, glm::vec3(1.0, 0.0, 0.0));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
-
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 18);
+	// 太阳自转的矩阵对地球没有用，所以先pop掉
+	mvStack.pop();
 
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 24);	// 0, 36, 24  (or 100000)
+	// 地球
+	mvStack.push(mvStack.top());
+	// 基于太阳公转的位置计算地球公转的位置，即计算出平移矩阵
+	mvStack.top() *= glm::translate(glm::mat4(1.0), glm::vec3(sin((float)currentTime) * 4.0, 0.0, cos((float)currentTime * 4.0)));
+	mvStack.push(mvStack.top());
+	// 基于地球的平移矩阵计算出实时位置的自转矩阵，即旋转矩阵
+	mvStack.top() *= glm::rotate(glm::mat4(1.0), (float)currentTime, glm::vec3(0.0, 1.0, 0.0));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	// pop掉地球的自转，这是对计算月球公转轨道是没用的
+	mvStack.pop();
+
+	// 月球
+	mvStack.push(mvStack.top());
+	// 在地球公转的基础上计算月球的公转、自转和缩放的模型视图矩阵
+	mvStack.top() *= glm::translate(glm::mat4(1.0), glm::vec3(0.0f, sin((float)currentTime) * 2.0, cos((float)currentTime) * 2.0));
+	mvStack.top() *= glm::rotate(glm::mat4(1.0), (float)currentTime, glm::vec3(0.0, 0.0, 1.0));
+	mvStack.top() *= glm::scale(glm::mat4(1.0), glm::vec3(0.25,0.25,0.25));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvStack.top()));
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	mvStack.pop();
+	mvStack.pop();
+	mvStack.pop();
+	mvStack.pop();
 }
 
 void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
@@ -99,7 +151,7 @@ int main(void) {
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 2", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 3", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
 	glfwSwapInterval(1);
